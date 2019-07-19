6399995dcbaf662fb2b67a0a6380470acb60e13b
ranjith8399 <53077689+ranjith8399@users.noreply.github.com>
2019-07-19 15:32:25 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
 #ranjith.ajusepoy88@gmail.com
 num=int(input())
-if (num%2)==0:
-    print('Even')
-elif (num%2)!=0:
-    print('Odd')
-else :
+if num<=0:
     print('Invalid')
+elif (num%2)==0:
+    print('Even')
+else :
+    print('Odd')
